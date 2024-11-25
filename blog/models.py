8eb2521c9dc8b3fc82a3acb86346700afd0b9b01
Paul Jebb <pjebb15@gmail.com>
2024-11-25 11:04:37 +0000

    Created Blog Post model, installed pillow for media upload handling.

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
-# Create your models here.
+# Blog Post Model
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='blog_images/', null=True, blank=True)
+
+    def __str__(self):
+        return self.title
+    
